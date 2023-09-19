27158e540df74e129eb8dd7939b627e3486643ee
rickCrz7 <cruzr@cslab-08.ad.cs.southwesternadventist.edu>
2023-09-19 15:04:41 -0500

    Added formating settings and Uno CSS

---
+++ src/models/types.ts
@@ -0,0 +1,20 @@
+export default interface Class {
+  id: number;
+  name: string;
+}
+
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  MiddleName: string;
+  classId: number;
+}
+
+export interface Teacher {
+  id: number;
+  firstName: string;
+  lastName: string;
+  MiddleName: string;
+  classId: number;
+}
